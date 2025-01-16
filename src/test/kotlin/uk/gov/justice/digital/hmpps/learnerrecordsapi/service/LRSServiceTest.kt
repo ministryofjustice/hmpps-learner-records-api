03354a16c6bd5f811682e3c33cfa99a17946b3b1
Ewan Donovan <ewan.donovan@digital.justice.com>
2025-01-16 13:30:42 +0000

    Add unit test for error

--- src/test/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/service/LRSServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/service/LRSServiceTest.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.learnerrecordsapi.service
 
 import kotlinx.coroutines.test.runTest
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
@@ -13,13 +16,13 @@ import org.mockito.kotlin.any
 import retrofit2.Response
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.config.AppConfig
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.config.HttpClientConfiguration
-import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.lrsapi.response.FindLearnerBody
-import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.lrsapi.response.FindLearnerEnvelope
-import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.lrsapi.response.FindLearnerResponse
-import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.lrsapi.response.Learner
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.lrsapi.response.*
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.lrsapi.response.exceptions.LRSException
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.request.FindLearnerByDemographicsRequest
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.response.FindLearnerByDemographicsResponse
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.response.LRSResponseType
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
 import java.time.LocalDate
 
 @ExtendWith(MockitoExtension::class)
@@ -159,5 +162,42 @@ class LRSServiceTest {
 
     assertEquals(expectedResult, result)
     verify(lrsApiServiceInterfaceMock).findLearnerByDemographics(any())
+  }
+
+  @Test
+  fun `should throw an LRSException when the API returns an error`(): Unit = runTest {
+    val requestBody = FindLearnerByDemographicsRequest(
+      givenName = "Some",
+      familyName = "Person",
+      dateOfBirth = LocalDate.of(1980, 1, 1),
+      gender = 1,
+      lastKnownPostCode = "ABCDEF",
+    )
+
+    val expectedException = LRSException(
+      MIAPAPIException(
+        errorCode = "WSEC0001",
+        errorActor = "uk.gov.miap.lrs.api.learner.utils.MSGValidator.validateSOAPMSG()",
+        description = "Invalid request, The request was badly formed - this may include missing mandatory parameters, or invalid data types.",
+        furtherDetails = "cvc-pattern-valid: Value '' is not facet-valid with respect to pattern '[\\s]*[\\S][\\s\\S]*' for type 'MIAPNameNonEmpty'.",
+        errorTimestamp = "2025-01-14 15:23:47",
+      ),
+    )
+
+    val inputStream = javaClass.classLoader.getResourceAsStream("error_ful.xml") ?: throw IllegalArgumentException("File not found in resources: error_ful.xml")
+
+    `when`(lrsApiServiceInterfaceMock.findLearnerByDemographics(any())).thenReturn(
+      Response.error(
+        500,
+        ResponseBody.create("text/xml".toMediaTypeOrNull(), InputStreamReader(inputStream, StandardCharsets.UTF_8).readText()),
+      ),
+    )
+
+    val actualException = assertThrows<LRSException> {
+      lrsService.findLearner(requestBody)
+    }
+
+    verify(lrsApiServiceInterfaceMock).findLearnerByDemographics(any())
+    assertEquals(expectedException.toString(), actualException.toString())
   }
 }
