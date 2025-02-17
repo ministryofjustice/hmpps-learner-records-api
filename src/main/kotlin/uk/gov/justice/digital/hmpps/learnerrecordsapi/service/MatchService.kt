71e918bacaa6e53bfdfef9a54ec38295610418ec
Ewan Donovan <ewan.donovan@digital.justice.co.uk>
2025-02-17 14:05:56 +0000

    Cleaning up

--- src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/service/MatchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/service/MatchService.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.learnerrecordsapi.service
 
-import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.db.MatchEntity
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.repository.MatchRepository
@@ -9,12 +8,6 @@ import uk.gov.justice.digital.hmpps.learnerrecordsapi.repository.MatchRepository
 class MatchService(
   private val matchRepository: MatchRepository,
 ) {
-
-  @PostConstruct
-  fun onStartup() {
-    println("Running a test call")
-    saveMatch(MatchEntity("ABCDEFGH", "1234567890"))
-  }
 
   fun findMatch(matchEntity: MatchEntity): MatchEntity? = matchRepository.findFirstByNomisIdOrderByIdDesc(
     matchEntity.nomisId,
