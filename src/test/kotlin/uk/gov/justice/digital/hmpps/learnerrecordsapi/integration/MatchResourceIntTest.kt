44caa1773140ef61b64ffc8bb27c25eb255c28e3
Ewan Donovan <ewan.donovan@digital.justice.co.uk>
2025-02-19 14:08:21 +0000

    mockito spying of service

--- src/test/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/integration/MatchResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/integration/MatchResourceIntTest.kt
@@ -2,18 +2,50 @@ package uk.gov.justice.digital.hmpps.learnerrecordsapi.integration
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.never
+import org.mockito.Mockito.reset
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.any
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.config.HmppsBoldLrsExceptionHandler
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.db.MatchEntity
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.request.ConfirmMatchRequest
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.response.MatchResponse
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.repository.MatchRepository
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.service.MatchService
 
 class MatchResourceIntTest : IntegrationTestBase() {
 
+  @TestConfiguration
+  class MockitoSpyConfig {
+
+    @Autowired
+    lateinit var matchRepository: MatchRepository
+
+    @Bean
+    fun matchService(): MatchService {
+      return spy(MatchService(matchRepository))
+    }
+  }
+
   @Autowired
   lateinit var objectMapper: ObjectMapper
+
+  @Autowired
+  lateinit var spiedMatchService: MatchService
+
+  @BeforeEach
+  fun setUp() {
+    reset(spiedMatchService) // Reset mock behavior before each test
+  }
 
   @Test
   fun `POST to confirm match should return 200 with a response confirming a match`() {
@@ -38,6 +70,7 @@ class MatchResourceIntTest : IntegrationTestBase() {
     val expectedSavedMatchEntity = confirmMatchRequest.asMatchEntity().copy(id = 1)
     val expectedResponse = MatchResponse("Match confirmed successfully", expectedSavedMatchEntity)
 
+    verify(spiedMatchService, times(1)).saveMatch(any())
     assertThat(actualResponse).isEqualTo(expectedResponse)
   }
 
@@ -69,6 +102,7 @@ class MatchResourceIntTest : IntegrationTestBase() {
       moreInfo = "Validation(s) failed for [nomisId] with reason(s): [must match \"^[A-Z]\\d{4}[A-Z]{2}\$\"]",
     )
 
+    verify(spiedMatchService, never()).saveMatch(any())
     assertThat(actualResponse).isEqualTo(expectedError)
   }
 
@@ -100,6 +134,7 @@ class MatchResourceIntTest : IntegrationTestBase() {
       moreInfo = "Validation(s) failed for [matchingUln] with reason(s): [must match \"^[0-9]{1,10}\$\"]",
     )
 
+    verify(spiedMatchService, never()).saveMatch(any())
     assertThat(actualResponse).isEqualTo(expectedError)
   }
 }
