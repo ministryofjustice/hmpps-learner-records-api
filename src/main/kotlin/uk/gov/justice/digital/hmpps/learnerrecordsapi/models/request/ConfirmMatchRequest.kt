acee87cd4e23308e5b805a62689732ec4c8a88e8
Ewan Donovan <ewan.donovan@digital.justice.co.uk>
2025-02-19 12:02:59 +0000

    remove useless annotation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/models/request/ConfirmMatchRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/models/request/ConfirmMatchRequest.kt
@@ -1,17 +1,14 @@
 package uk.gov.justice.digital.hmpps.learnerrecordsapi.models.request
 
-import com.google.gson.annotations.SerializedName
 import jakarta.validation.constraints.Pattern
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.db.MatchEntity
 
 data class ConfirmMatchRequest(
 
   @field:Pattern(regexp = "^[A-Z]\\d{4}[A-Z]{2}\$")
-  @SerializedName("nomisId")
   val nomisId: String,
 
   @field:Pattern(regexp = "^[0-9]{1,10}\$")
-  @SerializedName("uln")
   val matchingUln: String,
 ) {
   fun asMatchEntity() = MatchEntity(nomisId, matchingUln)
