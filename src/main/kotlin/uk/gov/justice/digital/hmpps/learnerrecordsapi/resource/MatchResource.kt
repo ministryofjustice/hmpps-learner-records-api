db0a21c2c35aac26779d8f04221ced813ceefd9e
Ewan Donovan <ewan.donovan@digital.justice.co.uk>
2025-02-19 11:34:53 +0000

    in progress

--- src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/resource/MatchResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/resource/MatchResource.kt
@@ -14,25 +14,26 @@ import uk.gov.justice.digital.hmpps.learnerrecordsapi.logging.LoggerUtil
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.logging.LoggerUtil.log
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.db.MatchEntity
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.request.ConfirmMatchRequest
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.response.MatchResponse
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.service.MatchService
 
 @RestController
 @PreAuthorize("hasRole('ROLE_LEARNER_RECORDS_SEARCH__RO')")
-@RequestMapping(value = ["/"], produces = ["application/json"])
+@RequestMapping(value = ["/match"], produces = ["application/json"])
 class MatchResource(
   private val matchService: MatchService
 ) {
 
   val logger = LoggerUtil.getLogger<MatchResource>()
 
-  @PostMapping(value = ["/confirm-match"])
+  @PostMapping(value = ["/confirm"])
   @Tag(name = "Match")
   suspend fun confirmMatch(
-    @RequestBody @Valid matchRequest: ConfirmMatchRequest,
-    @RequestHeader("X-Username", required = true) userName: String,
-  ): ResponseEntity<MatchEntity> {
-    logger.log("Received a post request to match endpoint", matchRequest)
-    val savedMatchEntity = matchService.saveMatch(matchRequest.asMatchEntity())
-    return ResponseEntity.status(HttpStatus.OK).body(savedMatchEntity)
+    @RequestBody @Valid confirmMatchRequest: ConfirmMatchRequest,
+  ): ResponseEntity<MatchResponse> {
+    logger.log("Received a post request to confirm match endpoint", confirmMatchRequest)
+    val savedMatchEntity = matchService.saveMatch(confirmMatchRequest.asMatchEntity())
+    val matchResponse = MatchResponse("Match confirmed successfully", savedMatchEntity)
+    return ResponseEntity.status(HttpStatus.OK).body(matchResponse)
   }
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/integration/MatchResourceIntTest.kt
@@ -0,0 +1,69 @@
+package uk.gov.justice.digital.hmpps.learnerrecordsapi.integration
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.any
+import org.mockito.Mockito.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.request.ConfirmMatchRequest
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.response.MatchResponse
+import uk.gov.justice.digital.hmpps.learnerrecordsapi.service.MatchService
+
+class MatchResourceIntTest : IntegrationTestBase() {
+
+  @Autowired
+  lateinit var objectMapper: ObjectMapper
+
+  @Test
+  fun `POST to confirm match should return 200 with a response confirming a match`() {
+    val confirmMatchRequest = ConfirmMatchRequest("ABCDEFGH", "1234567890")
+
+    val actualResponse = objectMapper.readValue(
+      webTestClient.post()
+        .uri("/match/confirm")
+        .headers(setAuthorisation(roles = listOf("ROLE_LEARNER_RECORDS_SEARCH__RO")))
+        .header("X-Username", "TestUser")
+        .bodyValue(confirmMatchRequest)
+        .accept(MediaType.parseMediaType("application/json"))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody()
+        .returnResult()
+        .responseBody,
+      MatchResponse::class.java,
+    )
+
+    val expectedSavedMatchEntity = confirmMatchRequest.asMatchEntity().copy(id = 1)
+    val expectedResponse = MatchResponse("Match confirmed successfully", expectedSavedMatchEntity)
+
+    assertThat(actualResponse).isEqualTo(expectedResponse)
+  }
+
+  fun `POST to confirm match should return 400 with appropriate error response`() {
+    val confirmMatchRequest = ConfirmMatchRequest("ABCDEFGH", "1234567890")
+
+    val actualResponse = objectMapper.readValue(
+      webTestClient.post()
+        .uri("/match/confirm")
+        .headers(setAuthorisation(roles = listOf("ROLE_LEARNER_RECORDS_SEARCH__RO")))
+        .header("X-Username", "TestUser")
+        .bodyValue(confirmMatchRequest)
+        .accept(MediaType.parseMediaType("application/json"))
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody()
+        .returnResult()
+        .responseBody,
+      MatchResponse::class.java,
+    )
+
+    val expectedSavedMatchEntity = confirmMatchRequest.asMatchEntity().copy(id = 1)
+    val expectedResponse = MatchResponse("Match confirmed successfully", expectedSavedMatchEntity)
+
+    assertThat(actualResponse).isEqualTo(expectedResponse)
+  }
+}
