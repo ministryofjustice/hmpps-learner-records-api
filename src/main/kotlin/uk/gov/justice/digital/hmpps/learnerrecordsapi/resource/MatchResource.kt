2c03466c6694bb92c01741d78b9a5f4508956faf
Ewan Donovan <ewan.donovan@digital.justice.co.uk>
2025-02-20 16:40:43 +0000

    klint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/resource/MatchResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/learnerrecordsapi/resource/MatchResource.kt
@@ -1,27 +1,16 @@
 package uk.gov.justice.digital.hmpps.learnerrecordsapi.resource
 
 import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.validation.ConstraintViolationException
 import jakarta.validation.Valid
-import jakarta.validation.constraints.Pattern
-import org.springframework.core.MethodParameter
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.validation.BindingResult
-import org.springframework.validation.ObjectError
-import org.springframework.web.bind.MethodArgumentNotValidException
-import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.context.request.WebRequest
-import org.springframework.web.method.annotation.HandlerMethodValidationException
-import uk.gov.justice.digital.hmpps.learnerrecordsapi.config.HmppsBoldLrsExceptionHandler.ErrorResponse
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.logging.LoggerUtil
-import uk.gov.justice.digital.hmpps.learnerrecordsapi.logging.LoggerUtil.errorLog
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.logging.LoggerUtil.log
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.request.MatchRequest
 import uk.gov.justice.digital.hmpps.learnerrecordsapi.models.response.MatchResponse
