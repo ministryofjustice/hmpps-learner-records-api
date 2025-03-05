a5f964bd8ec79cbaf15fc239b1f0ee84c9a3cc0a
Malawarayan Ganesan <malawarayan.ganesan@digital.justice.gov.uk>
2025-03-05 14:13:17 +0000

    fix(V2__alter_matches_table.sql): fix column name count of returned ulns

--- src/main/resources/db/migration/V2__alter_matches_table.sql
+++ src/main/resources/db/migration/V2__alter_matches_table.sql
@@ -1,3 +1,3 @@
 
 ALTER TABLE matches ADD COLUMN match_type VARCHAR(255) NOT NULL default 'NO_MATCH_SELECTED';
-ALTER TABLE matches ADD COLUMN count_of_matched_ulns VARCHAR(255) NOT NULL default '0';
+ALTER TABLE matches ADD COLUMN count_of_returned_ulns VARCHAR(255) NOT NULL default '0';
